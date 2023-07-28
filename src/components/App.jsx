7cfcd264afe71cd77705ebbb307c6d7575c33853
Arik <arikabdullah1122@gmail.com>
2023-07-28 12:56:41 -0400

    Testing ability to push to repository.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,21 +3,22 @@ import Header from "./Header";
 import WelcomeTypewriter from "./WelcomeTypewriter";
 import servers from "./servers/serverList";
 
+// TEST OF ABILITY TO PUSH
 function App() {
-  return (
-    <main className="App">
-      <Header />
-      <WelcomeTypewriter message="Welcome to WarServeR" />{" "}
-      <section className="home-intro">
-        <h1>What is WarServeR?</h1>
-        <p>
-          At WarServeR, we are keeping Quake 3 Arena alive for the all the
-          players that have stuck with it over the last 2 decades as well as the
-          next generation of players.
-        </p>
-      </section>
-    </main>
-  );
+	return (
+		<main className="App">
+			<Header />
+			<WelcomeTypewriter message="Welcome to WarServeR" />{" "}
+			<section className="home-intro">
+				<h1>What is WarServeR?</h1>
+				<p>
+					At WarServeR, we are keeping Quake 3 Arena alive for the all the
+					players that have stuck with it over the last 2 decades as well as the
+					next generation of players.
+				</p>
+			</section>
+		</main>
+	);
 }
 
 export default App;
